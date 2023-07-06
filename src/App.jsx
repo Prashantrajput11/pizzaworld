5a984f854c77fc4adb95b38dd8205204a3773cc6
Prashant Tanwar <69415658+Prashantrajput11@users.noreply.github.com>
2023-07-06 10:39:32 +0530

    feat : order fetch feature done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderloader } from "./features/order/Order";
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
 	{
 		element: <AppLayout />,
+		errorElement: <Error />,
 		children: [
 			{
 				path: "/",
@@ -28,8 +30,9 @@ const router = createBrowserRouter([
 				element: <CreateOrder />,
 			},
 			{
-				path: "/order/:/orderId",
+				path: "/order/:orderId",
 				element: <Order />,
+				loader: orderloader,
 			},
 		],
 	},
